f51527f792059d509d1b00905ad2daef42f9e52c
Michael Mulich <michael.mulich@gmail.com>
2016-09-28 12:06:47 -0700

    Factor the data directory logic into a fixture
    This isn't much code, but it will mean that it won't need repeated
    or imported on use in another test module.

--- cnxml/tests/test_validation.py
+++ cnxml/tests/test_validation.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
-from pathlib import Path
 
 
-here = Path(__file__).parent
-DATA_DIR = here / 'data'
-
-
-def test_validate_cnxml():
+def test_validate_cnxml(datadir):
     from cnxml import validate_cnxml
-    errors = validate_cnxml(DATA_DIR / 'valid.cnxml')
+    errors = validate_cnxml(datadir / 'valid.cnxml')
     assert errors == tuple()
 
 
-def test_cnxml_validation_messages():
+def test_cnxml_validation_messages(datadir):
     from cnxml import validate_cnxml
     expected = (
         ['30', '17', 'error', 'unfinished element'],
         ['55', '20', 'error', 'unfinished element'],
         ['67', '11', 'error', 'required attributes missing'],
     )
-    errors = validate_cnxml(DATA_DIR / 'invalid.cnxml')
+    errors = validate_cnxml(datadir / 'invalid.cnxml')
     assert errors == expected
